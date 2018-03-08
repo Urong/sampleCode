3ef2ddcf22fc4040512337a9b6f644a7ea7446d0
byeon0gam <dudwns519@daum.net>
2018-03-08 14:26:01 +0900

    test

--- JavaCollections/src/com/younggambyeon/test/TestMain.java
+++ JavaCollections/src/com/younggambyeon/test/TestMain.java
@@ -3,14 +3,23 @@ package com.younggambyeon.test;
 public class TestMain {
 
 	public static void main(String[] args) {
-		
+
 		TestInterface interfaceA = new TestClass();
-		
+
 		TestAbstract abstractA = new TestClass2();
 
 		System.out.println(abstractA.b);
 		System.out.println(abstractA.say());
 		System.out.println(abstractA.say2());
 		System.out.println(interfaceA.say());
+
+		String input = "토마토";
+
+		StringBuilder builder = new StringBuilder(input);
+		System.out.println(builder.reverse().toString());
+
+		char[] in = input.toCharArray();
+
+		System.out.println(in[1]);
 	}
 }
